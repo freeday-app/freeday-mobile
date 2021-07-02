0cd74715d1cc8615d2300ea60ac06bdd914825cd
Yoann <yoann@localhost.com>
2021-07-02 15:02:42 +0200

    lint fix

--- src/components/views/settings.js
+++ src/components/views/settings.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text } from 'react-native';
 
 import Page from '../organisms/page.js';
-
-const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center'
-    }
-});
 
 export default function Settings() {
     return (
